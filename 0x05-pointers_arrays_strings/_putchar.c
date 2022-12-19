81df8915e6a9466c7b575b011e215914c912f936
Catherine Namale <catherinenamale7@gmail.com>
2022-12-19 07:42:18 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+*putchar - writes the character c to standard
+*Returns: 1 on success
+*On error -1 is recorded and errno is set appropriately
+*/
+int _putchar(char c)
+{
+return (write (1 &c, 1));
+}
